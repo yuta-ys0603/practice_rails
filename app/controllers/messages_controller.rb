7e366207278bf36a2fda3878883d74f601deabef
yuta-ys0603 <yuta-ys0603>
2015-11-10 20:25:28 +0900

    change view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,68 +4,26 @@ class MessagesController < ApplicationController
   # GET /messages
   # GET /messages.json
   def index
-    @messages = Message.all
-  end
-
-  # GET /messages/1
-  # GET /messages/1.json
-  def show
-  end
-
-  # GET /messages/new
-  def new
     @message = Message.new
+    @messages = Message.all
+
   end
 
-  # GET /messages/1/edit
-  def edit
-  end
 
   # POST /messages
   # POST /messages.json
   def create
     @message = Message.new(message_params)
 
-    respond_to do |format|
       if @message.save
-        format.html { redirect_to @message, notice: 'Message was successfully created.' }
-        format.json { render :show, status: :created, location: @message }
+        redirect_to messages_url, notice: 'Message was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
+        redirect_to messages_url, alert: 'Message was unsuccessfully created.'
       end
     end
-  end
 
-  # PATCH/PUT /messages/1
-  # PATCH/PUT /messages/1.json
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to @message, notice: 'Message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /messages/1
-  # DELETE /messages/1.json
-  def destroy
-    @message.destroy
-    respond_to do |format|
-      format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_message
-      @message = Message.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
